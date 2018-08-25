8206248628575712f2d4391275ae4a52f169f5b3
BaleLin <bale.lin@oocl.com>
2018-08-25 12:24:03 +0800

    [bale]pass the test children regular and the dayRented is 3

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -65,4 +65,20 @@ public class CustomerTest {
 
     }
 
+    @Test
+    public void should_print_right_message_when_given_REGULAR_movice_and_3_dayRented(){
+
+        Movie movie = new Movie("Romance of the Three Kingdoms",Movie.REGULAR);
+        Rental rental = new Rental(movie,3);
+        Customer customer = new Customer("Jhon");
+
+        customer.addRental(rental);
+
+        assertEquals("Rental Record for Jhon\n" +
+                "movice title:Romance of the Three Kingdoms,this movice amount:3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points\n",customer.statement());
+
+    }
+
 }
